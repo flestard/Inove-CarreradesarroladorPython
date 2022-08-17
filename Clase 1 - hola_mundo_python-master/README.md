c91b50f30a22ad5af89d00c22648ef1b9854d30a
Facundo Jesús Lestard <80214430+flestard@users.noreply.github.com>
2022-08-17 13:18:36 -0300

    Add files via upload

---
+++ Clase 1 - hola_mundo_python-master/README.md
@@ -0,0 +1,10 @@
+![Inove banner](/inove.jpg)
+Inove Escuela de Código\
+info@inove.com.ar\
+Web: [Inove](http://inove.com.ar)
+
+# Hola Mundo! [Python]
+Programa creado para ensayar el correcto funcionamiento del entorno de instalación Python
+
+# Consultas
+alumnos@inove.com.ar
---
+++ Clase 1 - hola_mundo_python-master/hola_mundo.py
@@ -0,0 +1,11 @@
+# Hola Mundo! [Python]
+# ---------------------------
+# Autor: Inove Coding School
+# Version: 2.0
+
+# Descripcion:
+# Programa creado por Facundo Jesús Lestard para ensayar el correcto funcionamiento
+# del entorno de instalación Python
+
+
+print("Hola Mundo!")
